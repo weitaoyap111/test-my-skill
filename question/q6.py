d8f2df5ea9df24d11705888ce481763a8fb5ed4c
weitao <weitaoyap111@gmail.com>
2023-09-14 01:02:34 +0800

    fix q6.py bug

--- question/q6.py
+++ question/q6.py
@@ -1,27 +1,29 @@
-import math
+from pprint import pprint
 
 triangle_record = []
 
+
 def get_result_c(a, b):
-    result = math.sqrt(a**2 + b**2)
+    result = (a * a + b * b) ** 0.5
     return int(result) if result % 1 == 0 else -1
 
-max_value = 1001
-for x in range(1, max_value):
-    for y in range(1, x):
+
+max_value = 1000
+for x in range(1, max_value + 1):
+    for y in range(x, max_value + 1):
         flag = True
         value = get_result_c(x, y)
-        if value < max_value:
-            if value != -1:
-                if len(triangle_record) == 0:
-                    triangle_record.append([x, y, value])
-                else:
-                    for count, c in enumerate(triangle_record):
-                        if x % c[0] == 0 and y % c[1] == 0 and value % c[2] == 0:
-                            flag = False
-                            break
-                    if flag:
-                        triangle_record.append([x, y, value])
+        if value < max_value and value != -1:
+            if len(triangle_record) == 0:
+                triangle_record.append([x, y, value, 1])
+            else:
+                count = 0
+                for count, c in enumerate(triangle_record):
+                    if x / c[0] == y / c[1]:
+                        flag = False
+                        break
+                if flag:
+                    triangle_record.append([x, y, value, count+2])
 
-print("list of length(b, a, c):")
-print(triangle_record)
+print("list of length(a, b, c):")
+pprint(len(triangle_record))
